7d580b08ef31bcaedc6c45d795cf113a77b503dc
Lomosaurio123 <mhernandezl1605@alumno.ipn.mx>
2023-08-31 01:14:18 -0600

    Tarea 1

--- Tarea1.cpp
+++ Tarea1.cpp
@@ -2,31 +2,35 @@
 #include <vector>
 #include <string>
 
-std::vector<std::string> generateCombinations(int n) {
-    std::vector<std::vector<std::string>> dp(n + 1);
-    dp[1] = {"0", "1"};
+using namespace std;
+
+vector<string> generateCombinations(int n) {
+    vector< vector< string > > dp(n + 1);
+    dp[ 1 ] = {"0", "1"};
 
     for (int i = 2; i <= n; ++i) {
-        for (const std::string &prev : dp[i - 1]) {
+        for (const string &prev : dp[i - 1]) {
             dp[i].push_back(prev + "0");
             dp[i].push_back(prev + "1");
         }
     }
 
-    std::vector<std::string> combinations;
-    for (const std::vector<std::string> &lengthCombinations : dp) {
-        combinations.insert(combinations.end(), lengthCombinations.begin(), lengthCombinations.end());
+    vector<string> combinations;
+    for ( const vector< string > &lengthCombinations : dp ) {
+        
+        combinations.insert( combinations.end(), lengthCombinations.begin(), lengthCombinations.end() );
+        
     }
 
     return combinations;
 }
 
 int main() {
-    int n = 2;
-    std::vector<std::string> combinations = generateCombinations(n);
+    int n = 5;
+    vector< string > combinations = generateCombinations(n);
 
-    for (const std::string &combination : combinations) {
-        std::cout << combination << " ";
+    for (const string &combination : combinations) {
+        cout << combination << " ";
     }
 
     return 0;
